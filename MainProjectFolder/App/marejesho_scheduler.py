39c3da801293508ca42a914e0bf1449dedc83a48
dimosojunior <juniordimoso8@gmail.com>
2024-12-03 11:30:33 +0300

    new

--- MainProjectFolder/App/marejesho_scheduler.py
+++ MainProjectFolder/App/marejesho_scheduler.py
@@ -5,7 +5,7 @@ from .no_rejesho_scheduler import check_and_update_rejesho
 def start():
     scheduler = BackgroundScheduler()
     # Once a day at 6:00 PM
-    scheduler.add_job(check_and_update_rejesho, 'cron', hour=20, minute=0)
+    scheduler.add_job(check_and_update_rejesho, 'cron', hour=21, minute=0)
     
     # Uncomment the following line to schedule task every 10 seconds
     #scheduler.add_job(check_and_update_rejesho, 'interval', seconds=10)
